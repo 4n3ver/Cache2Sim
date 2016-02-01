3fdf7d505a81ac57f9584e5409ff74218c52a950
Yoel Ivan <yivan3@gatech.edu>
2016-02-01 10:24:59 -0500

    initial logic implementation

--- cachesim.cpp
+++ cachesim.cpp
@@ -1,37 +1,4 @@
 #include "cachesim.hpp"
-#include "cache.h"
-#include <deque>
-#include <stdlib.h>
-
-static cache *l1;
-static cache *l2;
-static uint64_t MAX_VC_SIZE;
-static std::deque<uint64_t> vc;
-
-static const int VC_MISS = 979;
-
-static void swap_victim(uint64_t target, block_ptr victim) {
-    DEBUG_ASSERT(!(victim->dirty));
-    target = target & ~(l1->getMask_off());
-    for (int i = 0; i < vc.size(); i++) {
-        if (vc[i] == target) {
-            // swapping and return
-            uint64_t temp = vc[i];
-            vc[i] = victim->address;
-            victim->address = temp;
-            DEBUG_ASSERT(victim->address & l1->getMask_off() == 0);
-            return;
-        }
-    }
-    // miss @ VC
-    DEBUG_ASSERT(vc.size() <= MAX_VC_SIZE);
-    if (vc.size() == MAX_VC_SIZE) {
-        vc.pop_front();
-    }
-    DEBUG_ASSERT(victim->address & l1->getMask_off() == 0);
-    vc.push_back(victim->address);
-    throw VC_MISS;
-}
 
 /**
  * Subroutine for initializing the cache. You many add and initialize any global or heap
@@ -48,17 +15,19 @@ static void swap_victim(uint64_t target, block_ptr victim) {
  * @s2 The number of blocks in each set of L2: 2^s2 blocks per set.
  * Note: c2 >= c1, b2 >= b1 and s2 >= s1.
  */
-void setup_cache(uint64_t c1, uint64_t b1, uint64_t s1, uint64_t v,
-                 uint64_t c2, uint64_t b2, uint64_t s2) {
+void setup_cache(
+        uint64_t c1, uint64_t b1, uint64_t s1, uint64_t v,
+        uint64_t c2, uint64_t b2, uint64_t s2) {
     if (c2 >= c1 && b2 >= b1 && s2 >= s1 && v <= 4 && v >= 0) {
-        l1 = new cache(c1, b1, s1);
-        l2 = new cache(c2, b2, s2);
+        l1 = new cache((int) c1, (int) b1, (int) s1);
+        l2 = new cache((int) c2, (int) b2, (int) s2);
         MAX_VC_SIZE = v;
         DEBUG_ASSERT(vc.size() == 0);
     } else {
         throw EINVAL;
     }
 }
+
 /**
  * Subroutine that simulates the cache one trace event at a time.
  * XXX: You're responsible for completing this routine
@@ -67,25 +36,14 @@ void setup_cache(uint64_t c1, uint64_t b1, uint64_t s1, uint64_t v,
  * @arg  The target memory address
  * @p_stats Pointer to the statistics structure
  */
-void cache_access(char type, uint64_t arg, cache_stats_t* p_stats) {
-    block_ptr target = nullptr;
-    try {
-        target = l1->access(arg);
-    } catch (int errno1) {
-        if (errno1 == cache::ACCESS_MISS) {
-            try {
-                target = l2->access(arg);
-            } catch (int errno2) {
-                 if (errno2 == cache::ACCESS_MISS) {
-
-                 } else {    // should not happen EVER
-                     system("exit");
-                 }
-            }
-        } else {    // should not happen EVER
-            system("exit");
-        }
+void cache_access(char type, uint64_t arg, cache_stats_t *p_stats) {
+    p_stats->accesses++;
+    if (type == READ) {
+        p_stats->reads++;
+    } else if (type == WRITE) {
+        p_stats->writes++;
     }
+    cache_access(l1, type, arg, p_stats, repair_l1_miss);
 }
 
 /**
@@ -98,4 +56,129 @@ void cache_access(char type, uint64_t arg, cache_stats_t* p_stats) {
 void complete_cache(cache_stats_t *p_stats) {
     delete l1;
     delete l2;
+}
+
+static void cache_access(
+        cache *cache_ptr, char type, uint64_t address,
+        cache_stats_t *p_stats, repair miss_repair) {
+    block_ptr blck = nullptr;
+    try {
+        blck = cache_ptr->access(address);
+    } catch (int errno_acc) {
+        if (errno_acc == cache::ACCESS_MISS) {
+            blck = miss_repair(type, address, p_stats);
+        } else {
+            SHOULD_NEVER_HAPPEN;
+        }
+    }
+    if (type == WRITE) {
+        blck->dirty = true;
+    }
+}
+
+static block_ptr repair_l1_miss(
+        char type, uint64_t address, cache_stats_t *p_stats) {
+    block_ptr blck = nullptr;
+    try {
+        blck = l1->find_empty_block(address);
+    } catch (int errno_empty) {
+        if (errno_empty == cache::SET_FULL) {
+            blck = l1->find_victim_block(address);
+        } else {
+            SHOULD_NEVER_HAPPEN;
+        }
+        if (blck->dirty) {
+            p_stats->write_back_l1++;
+            blck->dirty = false;
+            cache_access(l2, WRITE, blck->address, p_stats, repair_l2_miss);
+        }
+        blck->valid = false;
+        // I think we only use VC if we have a victim
+        uint64_t undead = EINVAL;
+        try {
+            undead = swap_victim(address, blck, p_stats);
+        } catch (int errno_swap) {
+            cache_access(l2, READ, address, p_stats, repair_l2_miss);
+        }
+        DEBUG_ASSERT(undead == address);
+    }
+    DEBUG_ASSERT(!(blck->valid));
+    DEBUG_ASSERT(!(blck->dirty));
+    *blck = {0};
+    l1->store_block(address, blck);
+    DEBUG_ASSERT(blck->valid);
+    if (type == READ) {
+        p_stats->read_misses_l1++;
+    } else if (type == WRITE) {
+        p_stats->write_misses_l1++;
+    } else {
+        SHOULD_NEVER_HAPPEN;
+    }
+    return blck;
+}
+
+
+static block_ptr repair_l2_miss(
+        char type, uint64_t address, cache_stats_t *p_stats) {
+    block_ptr blck = nullptr;
+    try {
+        blck = l2->find_empty_block(address);
+    } catch (int errno_empty) {
+        if (errno_empty == cache::SET_FULL) {
+            blck = l2->find_victim_block(address);
+        } else {
+            SHOULD_NEVER_HAPPEN;
+        }
+        if (blck->dirty) {
+            p_stats->write_back_l2++;
+            blck->dirty = false;
+            // TODO:WRITE BACK TO MEM
+        }
+        blck->valid = false;
+    }
+    DEBUG_ASSERT(!(blck->valid));
+    DEBUG_ASSERT(!(blck->dirty));
+    *blck = {0};
+    l2->store_block(address, blck);
+    DEBUG_ASSERT(blck->valid);
+    if (type == READ) {
+        p_stats->read_misses_l2++;
+    } else if (type == WRITE) {
+        p_stats->write_misses_l2++;
+    } else {
+        SHOULD_NEVER_HAPPEN;
+    }
+    return blck;
+}
+
+
+// what happen it is a HIT in VC and there are a empty block at L1,
+// no need swap in this case, or this will never happen?
+// start SWAPPING VC when the L1 IS FULL?
+// As long as we have free block in the L1 Cache, we dont need to check VC?
+static uint64_t swap_victim(
+        uint64_t target, block_ptr victim, cache_stats_t *p_stats) {
+    DEBUG_ASSERT(!(victim->dirty));
+    target = target & ~(l1->getMask_off());
+    for (int i = 0; i < vc.size(); i++) {
+        if (vc[i] == target) {
+            // swapping and return
+            p_stats->victim_hits++;
+            DEBUG_ASSERT(vc[i] & l1->getMask_off() == 0);
+            uint64_t temp = vc[i];
+            vc[i] = victim->address;
+            DEBUG_ASSERT(temp == target);
+            return temp;
+            // TODO: DONT FORGET TO CALL STORE AGAIN
+        }
+    }
+    // miss @ VC
+    DEBUG_ASSERT(vc.size() <= MAX_VC_SIZE);
+    if (vc.size() == MAX_VC_SIZE) {
+        vc.pop_front();
+    }
+    DEBUG_ASSERT(victim->address & l1->getMask_off() == 0);
+    vc.push_back(victim->address);
+    DEBUG_ASSERT(vc.size() <= MAX_VC_SIZE);
+    throw VC_MISS;
 }
--- cachesim.hpp
+++ cachesim.hpp
@@ -1,7 +1,12 @@
 #ifndef CACHESIM_HPP
 #define CACHESIM_HPP
 
+#define SHOULD_NEVER_HAPPEN system("exit")
+
 #include <cstdint>
+#include <deque>
+#include <stdlib.h>
+#include "cache.h"
 
 struct cache_stats_t {
     uint64_t accesses;
@@ -14,25 +19,56 @@ struct cache_stats_t {
     uint64_t write_back_l1;
     uint64_t write_back_l2;
     uint64_t victim_hits;
-    double   avg_access_time_l1;
+    double avg_access_time_l1;
 };
 
-void setup_cache(uint64_t c1, uint64_t b1, uint64_t s1, uint64_t v,
-                 uint64_t c2, uint64_t b2, uint64_t s2);
-void cache_access(char type, uint64_t arg, cache_stats_t* p_stats);
+typedef block_ptr (*repair)(char, uint64_t, cache_stats_t *);
+
+void setup_cache(
+        uint64_t c1, uint64_t b1, uint64_t s1, uint64_t v,
+        uint64_t c2, uint64_t b2, uint64_t s2);
+
+void cache_access(char type, uint64_t arg, cache_stats_t *p_stats);
+
 void complete_cache(cache_stats_t *p_stats);
 
-static const uint64_t DEFAULT_C1 = 12;   /* 4KB Cache */
-static const uint64_t DEFAULT_B1 = 5;    /* 32-byte blocks */
-static const uint64_t DEFAULT_S1 = 3;    /* 8 blocks per set */
-static const uint64_t DEFAULT_C2 = 15;   /* 32KB Cache */
-static const uint64_t DEFAULT_B2 = 5;    /* 32-byte blocks */
-static const uint64_t DEFAULT_S2 = 4;    /* 16 blocks per set */
-static const uint64_t DEFAULT_V =  3;    /* 8 blocks in VC */
+static const uint64_t DEFAULT_C1 = 12;
+/* 4KB Cache */
+static const uint64_t DEFAULT_B1 = 5;
+/* 32-byte blocks */
+static const uint64_t DEFAULT_S1 = 3;
+/* 8 blocks per set */
+static const uint64_t DEFAULT_C2 = 15;
+/* 32KB Cache */
+static const uint64_t DEFAULT_B2 = 5;
+/* 32-byte blocks */
+static const uint64_t DEFAULT_S2 = 4;
+/* 16 blocks per set */
+static const uint64_t DEFAULT_V = 3;    /* 8 blocks in VC */
 
 /** Argument to cache_access rw. Indicates a load */
-static const char     READ = 'r';
+static const char READ = 'r';
 /** Argument to cache_access rw. Indicates a store */
-static const char     WRITE = 'w';
+static const char WRITE = 'w';
+
+static const int VC_MISS = 979;
+
+static cache *l1;
+static cache *l2;
+static uint64_t MAX_VC_SIZE;
+static std::deque<uint64_t> vc;
+
+static uint64_t swap_victim(
+        uint64_t target, block_ptr victim, cache_stats_t *p_stats);
+
+static void cache_access(
+        cache *cache_ptr, char type, uint64_t address,
+        cache_stats_t *p_stats, repair miss_repair);
+
+static block_ptr repair_l1_miss(
+        char type, uint64_t address, cache_stats_t *p_stats);
+
+static block_ptr repair_l2_miss(
+        char type, uint64_t address, cache_stats_t *p_stats);
 
 #endif /* CACHESIM_HPP */
