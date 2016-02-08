0069ded93796847d9744daa17e15239108bf243c
Yoel Ivan <yivan3@gatech.edu>
2016-02-07 19:49:42 -0500

    minor output addition to test.sh and .gitignore, fix Makefile

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 rm -f *.log
 make clean; clear && clear
 make
+echo 'Running...'
 ./cachesim -c 12 -b 5 -s 3 -v 3 -C 15 -B 5 -S 4  < traces/astar.trace       2>&1 > astar.log; ./cachesim -c 12 -b 5 -s 0 -v 3 -C 15 -B 5 -S 4  < traces/bzip2.trace       2>&1 > bzip2.log; ./cachesim -c 12 -b 5 -s 3 -v 3 -C 15 -B 5 -S 10 < traces/mcf.trace         2>&1 > mcf.log; ./cachesim -c 12 -b 5 -s 3 -v 0 -C 15 -B 5 -S 4  < traces/perlbench.trace   2>&1 > perlbench.log
+echo 'Generating report...'
 make clean; python3 verify.py > verify.log
