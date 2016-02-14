b607a32bd46d9eaa9aa15db301beabfd27dc5ddf
Yoel Ivan <yivan3@gatech.edu>
2016-02-14 13:54:32 -0500

    optimized version @CacheConfigBFS

---
+++ CacheConfigBFS/cachesim.cpp
@@ -0,0 +1,271 @@
+/**
+ * Implementation file?
+ *
+ * @author  Yoel Ivan (yivan3@gatech.edu)
+ * @version 0.0a
+ */
+
+#include "cachesim.hpp"
+
+
+/* Max size of the Victim Cache in block, each block is as big as l1 cache, can be [0.4] */
+static uint64_t MAX_VC_SIZE;
+
+/* Hack so that the trace can be printed without too much mess. */
+typedef struct trace {
+    std::string stat_1;
+    std::string stat_2;
+    std::string stat_vc;
+
+    void reset() {
+//        stat_1 = "**";
+//        stat_2 = "**";
+//        stat_vc = "**";
+    }
+
+    trace() {
+        reset();
+    }
+
+    std::string get_trace() {
+//        std::string ret = stat_1 + (MAX_VC_SIZE ? stat_vc : "") + stat_2;
+//        reset();
+//        return ret;
+        return nullptr;
+    }
+
+    void hit_l1() {
+//        stat_1 = "H1";
+    }
+
+    void miss_l1() {
+//        stat_1 = "M1";
+    }
+
+    void hit_l2() {
+//        stat_2 = "H2";
+    }
+
+    void miss_l2() {
+//        stat_2 = "M2";
+    }
+
+    void hit_vc() {
+//        stat_vc = "HV";
+    }
+
+    void miss_vc() {
+//        stat_vc = "MV";
+    }
+} trace_t;
+
+static cache *l1;
+static cache *l2;
+static std::deque<uint64_t> vc;
+static trace_t last;
+
+static void cache_access(
+        cache *cache_ptr, char type, uint64_t address,
+        cache_stats_t *p_stats, repair miss_repair);
+
+static block_ptr repair_l1_miss(
+        char type, uint64_t address, cache_stats_t *p_stats);
+
+static block_ptr repair_l2_miss(
+        char type, uint64_t address, cache_stats_t *p_stats);
+
+static uint64_t vc_access(uint64_t target, cache_stats_t *p_stats) {
+    if (MAX_VC_SIZE) {
+        p_stats->accesses_vc++;
+        uint64_t offset_mask = ~(l1->getMask_off());
+        uint64_t masked_target = target & offset_mask;
+        for (std::deque<uint64_t>::iterator iter = vc.begin(); iter < vc.end();
+             iter++) {
+            if ((*iter & offset_mask) == masked_target) {
+                p_stats->victim_hits++;
+                target = *iter;
+                DEBUG_ASSERT((target & ~(l1->getMask_off())) == masked_target);
+                DEBUG_PRINT("[vc_access] VC_HIT %lx found\n", target);
+                vc.erase(iter);
+                last.hit_vc();
+                return target;
+            }
+        }
+        DEBUG_PRINT("[vc_access] VC_MISS\n");
+        last.miss_vc();
+    }
+    throw VC_MISS;
+}
+
+static void vc_push(uint64_t address) {
+    if (MAX_VC_SIZE) {
+        DEBUG_ASSERT(vc.size() <= MAX_VC_SIZE);
+        if (vc.size() == MAX_VC_SIZE) {
+            DEBUG_PRINT("[swap_victim] VC full, burying %lx\n", vc.front());
+            vc.pop_front();
+        }
+        DEBUG_PRINT("[swap_victim] pushing victim %lx\n", address);
+        vc.push_back(address);
+        DEBUG_ASSERT(vc.size() <= MAX_VC_SIZE);
+    }
+}
+
+/**
+ * Subroutine for initializing the cache. You many add and initialize any global or heap
+ * variables as needed.
+ * XXX: You're responsible for completing this routine
+ *
+ * @c1 The total number of bytes for data storage in L1 is 2^c1
+ * @b1 The size of L1's blocks in bytes: 2^b1-byte blocks.
+ * @s1 The number of blocks in each set of L1: 2^s1 blocks per set.
+ * @v Victim Cache's total number of blocks (blocks are of size 2^b1).
+ *    v is in [0, 4].
+ * @c2 The total number of bytes for data storage in L2 is 2^c2
+ * @b2 The size of L2's blocks in bytes: 2^b2-byte blocks.
+ * @s2 The number of blocks in each set of L2: 2^s2 blocks per set.
+ * Note: c2 >= c1, b2 >= b1 and s2 >= s1.
+ */
+void setup_cache(
+        uint64_t c1, uint64_t b1, uint64_t s1, uint64_t v,
+        uint64_t c2, uint64_t b2, uint64_t s2) {
+    if (c2 >= c1 && b2 >= b1 && s2 >= s1 && v <= 4 && v >= 0) {
+        l1 = new cache((int) c1, (int) b1, (int) s1);
+        l2 = new cache((int) c2, (int) b2, (int) s2);
+        MAX_VC_SIZE = v;
+    } else {
+        throw EINVAL;
+    }
+}
+
+/**
+ * Subroutine that simulates the cache one trace event at a time.
+ * XXX: You're responsible for completing this routine
+ *
+ * @type The type of event, can be READ or WRITE.
+ * @arg  The target memory address
+ * @p_stats Pointer to the statistics structure
+ */
+void cache_access(char type, uint64_t arg, cache_stats_t *p_stats) {
+    p_stats->accesses++;
+    if (type == READ) {
+        p_stats->reads++;
+    } else if (type == WRITE) {
+        p_stats->writes++;
+    }
+    cache_access(l1, type, arg, p_stats, repair_l1_miss);
+    TRACE_PRINT;
+}
+
+/**
+ * Subroutine for cleaning up any outstanding memory operations and calculating overall statistics
+ * such as miss rate or average access time.
+ * XXX: You're responsible for completing this routine
+ *
+ * @p_stats Pointer to the statistics structure
+ */
+void complete_cache(cache_stats_t *p_stats) {
+    delete l1;
+    delete l2;
+
+    double ht1 = 2 + 0.2 * l1->get_s();
+    double ht2 = 4 + 0.4 * l2->get_s();
+
+    double mr1 = (p_stats->read_misses_l1 + p_stats->write_misses_l1) /
+                 ((double) p_stats->accesses);
+    double mr2 = (p_stats->read_misses_l2 + p_stats->write_misses_l2) /
+                 ((double) p_stats->accesses_l2);
+
+    double aat2 = ht2 + mr2 * 500;
+
+    double mp1 = aat2 * (MAX_VC_SIZE == 0 ? 1 : (
+            (p_stats->accesses_vc - p_stats->victim_hits) /
+            ((double) p_stats->accesses_vc)
+    ));
+
+    p_stats->avg_access_time_l1 = ht1 + mr1 * mp1;
+}
+
+// ADDITION BELOW
+/**
+ * Comment on the prototype.
+ */
+
+static void cache_access(
+        cache *cache_ptr, char type, uint64_t address,
+        cache_stats_t *p_stats, repair miss_repair) {
+    block_ptr blck = nullptr;
+    try {
+        blck = cache_ptr->access(address);
+        if (cache_ptr == l1) {
+            last.hit_l1();
+        } else if (cache_ptr == l2 && type == READ) {
+            last.hit_l2();
+        }
+    } catch (int errno_acc) {
+        blck = miss_repair(type, address, p_stats);
+    }
+    if (type == WRITE) {
+        blck->dirty = true;
+    }
+}
+
+static block_ptr repair_l1_miss(
+        char type, uint64_t address, cache_stats_t *p_stats) {
+    try {
+        address = vc_access(address, p_stats);
+    } catch (int errno_vc) {
+        p_stats->accesses_l2++;
+        cache_access(l2, READ, address, p_stats, repair_l2_miss);
+    }
+    block_ptr blck = nullptr;
+    try {
+        blck = l1->find_empty_block(address);
+    } catch (int errno_full) {
+        blck = l1->find_victim_block(address);
+    }
+    if (blck->valid) {
+        if (blck->dirty) {
+            p_stats->write_back_l1++;
+            blck->dirty = false;
+            p_stats->accesses_l2++;
+            cache_access(l2, WRITE, blck->address, p_stats, repair_l2_miss);
+        }
+        blck->valid = false;
+        vc_push(blck->address);
+    }
+    l1->store_block(address, blck);
+    if (type == READ) {
+        p_stats->read_misses_l1++;
+    } else {
+        p_stats->write_misses_l1++;
+    }
+    last.miss_l1();
+    return blck;
+}
+
+static block_ptr repair_l2_miss(
+        char type, uint64_t address, cache_stats_t *p_stats) {
+    block_ptr blck = nullptr;
+    try {
+        blck = l2->find_empty_block(address);
+    } catch (int errno_full) {
+        blck = l2->find_victim_block(address);
+    }
+    if (blck->valid) {
+        if (blck->dirty) {
+            p_stats->write_back_l2++;
+            blck->dirty = false;
+        }
+        blck->valid = false;
+    }
+    l2->store_block(address, blck);
+    if (type == READ) {
+        p_stats->read_misses_l2++;
+        last.miss_l2();
+    } else {
+        p_stats->write_misses_l2++;
+    }
+    return blck;
+}
+
+// ADDITION ABOVE
---
+++ CacheConfigBFS/cachesim.hpp
@@ -0,0 +1,71 @@
+/**
+ * Handle high level logic of the multi-level cache (just 2-level) operation.
+ *
+ * @author  Yoel Ivan (yivan3@gatech.edu)
+ * @version 0.0a
+ */
+
+#ifndef CACHESIM_HPP
+#define CACHESIM_HPP
+
+#ifdef CCOMPILER
+#include <stdint.h>
+#else
+#include <cstdint>
+#endif
+
+#include <cstdlib>
+#include <iostream>
+#include <deque>
+#include "cache.h"
+
+//#define TRACE
+#ifdef TRACE
+    #define TRACE_PRINT std::cout << last.get_trace() << std::endl
+    #define SHOULD_NEVER_HAPPEN system("exit")
+#else
+    #define TRACE_PRINT
+    #define SHOULD_NEVER_HAPPEN
+#endif
+
+struct cache_stats_t {
+    uint64_t accesses;
+    uint64_t accesses_l2;
+    uint64_t accesses_vc;
+    uint64_t reads;
+    uint64_t read_misses_l1;
+    uint64_t read_misses_l2;
+    uint64_t writes;
+    uint64_t write_misses_l1;
+    uint64_t write_misses_l2;
+    uint64_t write_back_l1;
+    uint64_t write_back_l2;
+    uint64_t victim_hits;
+    double   avg_access_time_l1;
+};
+
+void setup_cache(uint64_t c1, uint64_t b1, uint64_t s1, uint64_t v,
+                 uint64_t c2, uint64_t b2, uint64_t s2);
+void cache_access(char type, uint64_t arg, cache_stats_t* p_stats);
+void complete_cache(cache_stats_t *p_stats);
+
+static const uint64_t DEFAULT_C1 = 12;   /* 4KB Cache */
+static const uint64_t DEFAULT_B1 = 5;    /* 32-byte blocks */
+static const uint64_t DEFAULT_S1 = 3;    /* 8 blocks per set */
+static const uint64_t DEFAULT_C2 = 15;   /* 32KB Cache */
+static const uint64_t DEFAULT_B2 = 5;    /* 32-byte blocks */
+static const uint64_t DEFAULT_S2 = 4;    /* 16 blocks per set */
+static const uint64_t DEFAULT_V =  3;    /* 3 blocks in VC */
+
+/** Argument to cache_access rw. Indicates a load */
+static const char     READ = 'r';
+/** Argument to cache_access rw. Indicates a store */
+static const char     WRITE = 'w';
+
+// ADDITION BELOW
+typedef block_ptr (*repair)(char, uint64_t, cache_stats_t *);
+
+const int VC_MISS = 979;
+// ADDITION ABOVE
+
+#endif /* CACHESIM_HPP */
