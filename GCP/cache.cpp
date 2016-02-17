c494c50751917b8aef0ed4bb4a36fc6e0537a69f
Yoel Ivan <yivan3@gatech.edu>
2016-02-17 07:35:50 +0000

    add cloud bfs result for mcf and perlbench

---
+++ GCP/cache.cpp
@@ -0,0 +1,143 @@
+/**
+ * Comment on the header file.
+ */
+
+#include <string.h>
+#include "cache.h"
+
+block_ptr cache::fetch_block(uint64_t address,
+                             std::function<bool (block_ptr)> cond) {
+    uint64_t index = index_of(address);
+
+    block_ptr set = data[index];
+    for (uint64_t i = 0; i < lim_set; i++) {
+        if (cond(&set[i])) {
+            return &set[i];
+        }
+    }
+    return nullptr;
+}
+
+cache::cache(int c, int b, int s) {
+    /* TODO: add more condition to ensure valid args */
+    if (c < 64 && c >= 0 && b + s <= c && b >= 0 && s >= 0) {
+        this->s = s;
+        shift_index = (uint8_t) b;
+        shift_tag = (uint8_t) (c - s);
+
+        lim_off = ((uint64_t) 1) << b;
+        lim_index = ((uint64_t) 1) << (c - (b + s));
+        lim_tag = ((uint64_t) 1) << (64 - (c - s));
+
+        mask_off = lim_off - 1;
+        mask_index = lim_index - 1;
+        mask_tag = lim_tag - 1;
+
+        lim_set = ((uint64_t) 1) << s;
+
+        data = new block_ptr[lim_index];
+        for (uint64_t i = 0; i < lim_index; i++) {
+            data[i] = new block[lim_set];
+        }
+
+//        if (DEBUG_IF(s == 0)) {
+//            DEBUG_PRINT("Direct-mapped cache\n");
+//        } else if (DEBUG_IF(s == c - b)) {
+//            DEBUG_PRINT("Fully-associative cache\n");
+//        } else {
+//            DEBUG_PRINT("%d-way associative cache\n", 1 << s);
+//        }
+//
+//        DEBUG_PRINT("C: %d B: %d S: %d\n", c, b, s);
+//
+//        DEBUG_PRINT("Cache size: %lu Byte\n", ((uint64_t) 1) << c);
+//        DEBUG_PRINT("Block size: %lu Byte\n", ((uint64_t) 1) << b);
+//        DEBUG_PRINT("Set size: %lu Block/line\n", lim_set);
+//
+//        DEBUG_PRINT("Offset mask: %lx\n", mask_off);
+//        DEBUG_PRINT("Index mask: %lx\n", mask_index);
+//        DEBUG_PRINT("Tag mask: %lx\n", mask_tag);
+//
+//        DEBUG_PRINT("Index shift: %d\n", shift_index);
+//        DEBUG_PRINT("Tag shift: %d\n", shift_tag);
+//
+//        for (uint64_t index = 0; index < lim_index; index++) {
+//            for (uint64_t set = 0; set < lim_set; set++) {
+//                DEBUG_ASSERT(data[index][set].tag == 0);
+//                DEBUG_ASSERT(data[index][set].address == 0);
+//                DEBUG_ASSERT(data[index][set].last_access_time == 0);
+//                DEBUG_ASSERT(!data[index][set].dirty);
+//                DEBUG_ASSERT(!data[index][set].valid);
+//            }
+//        }
+//        DEBUG_PRINT("\nInit test passed...\n");
+        clock = 0;
+    } else {
+        throw EINVAL;
+    }
+
+}
+
+cache::~cache() {
+    for (uint64_t i = 0; i < lim_index; i++) {
+        delete data[i];
+    }
+    delete data;
+}
+
+// row == index
+// col == set
+block_ptr cache::access(uint64_t address) {
+    DEBUG_ASSERT(clock < UINT64_MAX);
+    clock++;
+    uint64_t tag = tag_of(address);
+    block_ptr ret = fetch_block(address, [tag](block_ptr blck) -> bool {
+        return blck->valid && blck->tag == tag;
+    });
+
+    if (ret != nullptr) {
+        ret->last_access_time = clock;
+        DEBUG_PRINT("[acccess] access %lx to HIT\n", address);
+        return ret;
+    } else {
+        DEBUG_PRINT("[acccess] access %lx to MISS\n", address);
+        throw ACCESS_MISS;
+    }
+}
+
+void cache::store_block(uint64_t address, block_ptr location) {
+    location->reset();
+    location->tag = tag_of(address);
+    location->address = address;
+    location->last_access_time = clock;
+    location->valid = true;
+}
+
+block_ptr cache::find_victim_block(uint64_t address) {
+    uint64_t index = index_of(address);
+
+    block_ptr set = data[index];
+    block_ptr lru = &set[0];
+    uint64_t min = lru->last_access_time;
+    for (uint64_t i = 1; i < lim_set; i++) {
+        if (min > set[i].last_access_time) {
+            lru = &set[i];
+            min = lru->last_access_time;
+        }
+    }
+    DEBUG_PRINT("[find_victim_block] evicting %lx for %lx\n", lru->address, address);
+    return lru;
+}
+
+block_ptr cache::find_empty_block(uint64_t address) {
+    block_ptr ret = fetch_block(address, [](block_ptr blck) -> bool {
+        return !(blck->valid);
+    });
+
+    if (ret != nullptr) {
+        return ret;
+    } else {
+        DEBUG_PRINT("[find_empty_block] SET FULL\n");
+        throw SET_FULL;
+    }
+}
---
+++ GCP/cache.h
@@ -0,0 +1,154 @@
+/**
+ * Header file of the not-so-object-oriented cache object
+ *
+ * @author  Yoel Ivan (yivan3@gatech.edu)
+ * @version 0.0a
+ */
+
+#ifndef CACHE2SIM_CACHE_H
+#define CACHE2SIM_CACHE_H
+
+#include <string>
+#include <errno.h>
+#include <stdint.h>
+#include <functional>
+
+#ifdef DEBUG_CACHE
+    #include <stdio.h>
+    #include <assert.h>
+
+    #define DEBUG_PRINT(...) fprintf(stderr, __VA_ARGS__)
+    #define DEBUG_IF(x) x
+    #define DEBUG_ASSERT(x) assert(x)
+#else
+    #define DEBUG_PRINT(...)
+    #define DEBUG_IF(x) false
+    #define DEBUG_ASSERT(x)
+#endif
+
+/* Simple block struct that are not supposed to be exposed to user, but... */
+typedef struct s_block {
+    uint64_t tag;
+    uint64_t last_access_time;
+    uint64_t address;
+    bool dirty;
+    bool valid;
+
+    s_block() {
+        reset();
+    }
+
+    void reset() {
+        tag = 0;
+        last_access_time = 0;
+        address = 0;
+        dirty = false;
+        valid = false;
+    }
+} block, *block_ptr;
+
+/* Cache object, consists of 2-d array of block, row is index, col is set */
+class cache {
+
+    /* This is private. */
+    private:
+        uint64_t clock;
+
+        uint8_t shift_index;
+        uint8_t shift_tag;
+
+        uint64_t lim_off;
+        uint64_t lim_index;
+        uint64_t lim_tag;
+        uint64_t lim_set;
+
+        uint64_t mask_off;
+        uint64_t mask_index;
+        uint64_t mask_tag;
+
+        block_ptr *data;
+
+        int s;
+
+        block_ptr fetch_block(uint64_t address,
+                              std::function<bool (block_ptr)> cond);
+
+        uint64_t index_of(uint64_t address) {
+            return (address >> shift_index) & mask_index;
+        }
+
+        uint64_t tag_of(uint64_t address) {
+            return (address >> shift_tag) & mask_tag;
+        }
+
+        uint64_t offset_of(uint64_t address) {
+            return address & mask_off;
+        }
+
+    public:
+        /* Error code so user have something to throw in case of exception */
+        static const int ACCESS_MISS = 99;
+        static const int SET_FULL = 1099;
+
+        /**
+         * Constructor of this object
+         *
+         * @c   log base 2 of the size of the cache in byte
+         * @b   log base 2 of the size of each block in byte
+         * @s   log base 2 of the size of each set in block
+         */
+        cache(int c, int b, int s);
+
+        /**
+         * Return this cache hit time, based on log base 2 of the size of each set in block
+         */
+        int get_s() {
+            return s;
+        }
+
+        /**
+         * Gives mask to hide the offset based on this cache block size.
+         *
+         * @return  mask to hide the offset (zero out the offset by using bitwise &).
+         */
+        uint64_t getMask_off() const {
+            return mask_off;
+        }
+
+        /**
+         * Access this cache.
+         *
+         * @address address of the block to look for
+         * @throw   ACCESS_MISS if block not in this cache
+         * @return  block where @address passed reside.
+         */
+        block_ptr access(uint64_t address);
+
+        /**
+         * Find empty block where @address can be placed.
+         *
+         * @address address of the block to look for
+         * @return  empty block where @address can be placed
+         */
+        block_ptr find_empty_block(uint64_t address);
+
+        /**
+         * Store @address at @location
+         *
+         * @address     address to store
+         * @location    location in the cache where @address will be stored
+         */
+        void store_block(uint64_t address, block_ptr location);
+
+        /**
+         * Find LRU block based on @address.
+         *
+         * @address address that need to be placed in the LRU block, once the LRU block evicted
+         * @return  LRU block to be evicted
+         */
+        block_ptr find_victim_block(uint64_t address);
+
+        /* Plain old destructor */
+        ~cache();
+};
+#endif //CACHE2SIM_CACHE_H
